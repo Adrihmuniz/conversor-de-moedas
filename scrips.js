8813edffe8f6a4515b2284a384601f534b4b28a4
Adrihmuniz <124946206+Adrihmuniz@users.noreply.github.com>
2024-02-03 16:15:48 -0300

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -1,78 +1,64 @@
-const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConverted = document.querySelector(".currency-value")
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+    const currencyValueConverted = document.querySelector(".currency-value");
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
-    const libraToday = 6.29
-    const wonToday = 0.0037
+    const dolarToday = 5.2;
+    const euroToday = 6.2;
+    const libraToday = 6.29;
+    const wonToday = 0.0037;
 
     if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
-    }
-
-    if (currencySelect.value == "euro") {
+        }).format(inputCurrencyValue / dolarToday);
+    } else if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
-    }
-
-    if (currencySelect.value == "libra") {
+        }).format(inputCurrencyValue / euroToday);
+    } else if (currencySelect.value == "libra") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "GBP"
-        }).format(inputCurrencyValue / libraToday)
-    }
-
-    if (currencySelect.value == "won") {
+        }).format(inputCurrencyValue / libraToday);
+    } else if (currencySelect.value == "won") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("ko-KR", {
             style: "currency",
             currency: "KRW"
-        }).format(inputCurrencyValue / wonToday)
+        }).format(inputCurrencyValue / wonToday);
     }
-
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
-    }).format(inputCurrencyValue)
+    }).format(inputCurrencyValue);
 }
 
-function changeCurrency(){
-    const currencyName = document.getElementById("currency-name")
-    const currencyImage = document.querySelector(".currency-img")
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name");
+    const currencyImage = document.querySelector(".currency-img");
 
     if (currencySelect.value == "dolar") {
-    currencyName.innerHTML = "Dólar americano"
-    currencyImage.src = "./assets/dolar.png"
+        currencyName.innerHTML = "Dólar americano";
+        currencyImage.src = "./assets/dolar.png";
+    } else if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro";
+        currencyImage.src = "./assets/euro.png";
+    } else if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra";
+        currencyImage.src = "./assets/libra.png";
+    } else if (currencySelect.value == "won") {
+        currencyName.innerHTML = "Won";
+        currencyImage.src = "./assets/korea.png";
+    }
+
+    convertValues();
 }
 
-if (currencySelect.value == "euro") {
-    currencyName.innerHTML = "Euro"
-    currencyImage.src = "./assets/euro.png"
-}
-
-if (currencySelect.value == "libra") {
-    currencyName.innerHTML = "Libra"
-    currencyImage.src = "./assets/libra.png"
-}
-
-if (currencySelect.value == "won") {
-    currencyName.innerHTML = "won"
-    currencyImage.src = "./assets/korea.png"
-}
-
-    convertValues()
-
-}
-
-currencySelect.addEventListener("change", changeCurrency)
-convertButton.addEventListener("click", convertValues)
+currencySelect.addEventListener("change", changeCurrency);
+convertButton.addEventListener("click", convertValues);
